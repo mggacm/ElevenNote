4e3661c538047e73e5a1b2444a260688685241e6
Matthew Milstead <mattgmilstead@gmail.com>
2017-04-12 15:41:49 -0400

    Adding Create

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -7,11 +7,28 @@ using System.Web.Mvc;
 
 namespace ElevenNote.Web.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         public ActionResult Index()
         {
             var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
