51b665e95e3b4006b9a179b9db804f4c085ed741
Matthew Milstead <mattgmilstead@gmail.com>
2017-04-13 14:15:49 -0400

    Adding a Delete

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -91,6 +91,30 @@ namespace ElevenNote.Web.Controllers
             return View(model);
         }
 
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNoteService();
+
+            //TODO: handle failure
+            service.DeleteNote(id);
+
+            TempData["SaveResult"] = "Your note was deleted.";
+
+            return RedirectToAction("Index");
+        }
+
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
